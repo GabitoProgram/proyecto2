9d2995fc273d834ef39c670c5e25b63524a76066
GabitoProgram <153461149+GabitoProgram@users.noreply.github.com>
2024-12-09 22:55:12 -0400

    subida

--- src/componenteContenido/Titulo.jsx
+++ src/componenteContenido/Titulo.jsx
@@ -7,7 +7,7 @@ const Titulo = () => {
       <span className="w-[50%] flex flex-col place-content-center items-center">
         <div>
           <h1 className=" text-white text-[24px] leading-none inria-serif-bold">Hola, somos:</h1>
-          <h1 className=" text-[#B5B436] text-[96px] leading-none inria-serif-bold">FullStock</h1>
+          <h1 className=" text-[#B5B436] text-[96px] leading-none inria-serif-bold">LaSemillita</h1>
           <h1 className=" text-[#9492D2] text-[32px] leading-none py-2 inria-serif-bold">Calidad para tu hogar</h1>
           <h1 className=" text-[#B1BACC] text-[20px] leading-none py-4 inria-serif-regular">Somos un supermercado que abre sus puertas,<br/> donde te ofrece productos de calidad a <br/>precios razonables. </h1>
         </div>
