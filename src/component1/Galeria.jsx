9ebb852018ec223df4204f7fc9c612248988c731
GabitoProgram <153461149+GabitoProgram@users.noreply.github.com>
2024-12-11 02:05:31 -0400

    video

--- src/component1/Galeria.jsx
+++ src/component1/Galeria.jsx
@@ -5,9 +5,12 @@ const Galeria = () => {
         <div>
             <Barra_navegacionV2Galeria ruta={'Video'}/>
             <div className='flex items-center place-content-center h-screen w-full bg-[#0A192E]'>
-            <iframe width="860" height="515" src="https://www.youtube-nocookie.com/embed/-2suCyPpylg?si=NV91bvkvBJbpCTyx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="1000" height="700" src="https://www.youtube.com/embed/vIKcEM05jYY?si=p4Pz41jY4oFw4qUr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            
+            
             </div>
-    
+
         </div>
   )
 }
